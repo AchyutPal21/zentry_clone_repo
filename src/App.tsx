81bc86c52a1408a95d22668a425bcb2577f29faf
Achyut Pal <achyut21pal@gmail.com>
2024-12-18 20:56:46 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <main>
+      <h1 className="text-5xl text-violate-300 font-bold text-center font-circular-web">Let's Start The DEV!!!</h1>
+    </main>
+  )
+}
+
+export default App
